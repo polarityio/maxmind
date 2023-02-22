98a6596bbae5fc32c0e44662d446bb34847af55c
ntsmith7 <nick@polarity.io>
2023-02-22 13:36:46 -0800

    chore: minor version bump

--- components/maxmind-block.js
+++ components/maxmind-block.js
@@ -1,5 +1,42 @@
 'use strict';
 
 polarity.export = PolarityComponent.extend({
-  details: Ember.computed.alias('block.data.details')
+  details: Ember.computed.alias('block.data.details'),
+  showCopyMessage: false,
+  uniqueIdPrefix: '',
+  actions: {
+    copyData: function () {
+      Ember.run.scheduleOnce(
+        'afterRender',
+        this,
+        this.copyElementToClipboard,
+        `maxmind-container-${this.get('uniqueIdPrefix')}`
+      );
+
+      Ember.run.scheduleOnce('destroy', this, this.restoreCopyState);
+    }
+  },
+  copyElementToClipboard (element) {
+    window.getSelection().removeAllRanges();
+    let range = document.createRange();
+
+    range.selectNode(typeof element === 'string' ? document.getElementById(element) : element);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  },
+  getElementRance (element) {
+    let range = document.createRange();
+    range.selectNode(typeof element === 'string' ? document.getElementById(element) : element);
+    return range;
+  },
+  restoreCopyState () {
+    this.set('showCopyMessage', true);
+
+    setTimeout(() => {
+      if (!this.isDestroyed) {
+        this.set('showCopyMessage', false);
+      }
+    }, 2000);
+  }
 });
